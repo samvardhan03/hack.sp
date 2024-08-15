343b02750c8cbffbdd1f2b968d9a25fdd5cc559a
lighting129 <devrajajoo@gmail.com>
2024-08-15 22:22:00 +0530

    /

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import "./styles/home.css";
+import "./home.css";
 
 const Home = () => {
   return (
